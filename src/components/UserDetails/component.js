4d0225d147b150046ecffaab3b0c9bd3ef137703
nghianek123 <2051052089nghia@ou.edu.vn>
2024-05-21 21:26:59 +0700

    update UI header

--- src/components/UserDetails/component.js
+++ src/components/UserDetails/component.js
@@ -1,18 +1,22 @@
-import React from "react";
-import PropTypes from "prop-types";
-import "./UserDetails.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import './UserDetails.css';
 
 const UserDetails = ({ userImage, displayName }) => (
   <div className="user-details-container">
+    <div className="livestream" onClick={startLivestream}>Livestream</div>
     <img alt="user" className="user-image" src={userImage} />
     <p className="user-name">{displayName}</p>
   </div>
 );
 
-
 UserDetails.propTypes = {
   userImage: PropTypes.string,
-  displayName: PropTypes.string
+  displayName: PropTypes.string,
+};
+
+const startLivestream = () => {
+  console.log('aaa')
 };
 
 export default UserDetails;
