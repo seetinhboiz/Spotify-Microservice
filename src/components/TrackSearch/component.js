fc9fdb1de07275f20c1fd69fb741863cc850b2f0
HyTran312 <2051052061hy@ou.edu.vn>
2024-05-21 20:44:41 +0700

    add voice search

--- src/components/TrackSearch/component.js
+++ src/components/TrackSearch/component.js
@@ -13,6 +13,33 @@ class TrackSearch extends Component {
     });
   };
 
+  startVoiceRecognition = () => {
+    if (!('webkitSpeechRecognition' in window)) {
+      alert("Trình duyệt của bạn không hỗ trợ tính năng nhận dạng giọng nói.");
+      return;
+    }
+
+    const recognition = new window.webkitSpeechRecognition();
+    recognition.interimResults = true;
+    recognition.lang = 'vi-VN'; // Thiết lập ngôn ngữ nhận dạng
+
+    recognition.addEventListener('result', e => {
+      const transcript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('');
+
+      this.setState({ searchTerm: transcript });
+    });
+
+    recognition.addEventListener('end', () => {
+      recognition.stop();
+      this.props.searchSongs(this.state.searchTerm, this.props.token);
+    });
+
+    recognition.start();
+  };
+
   render() {
     return (
       <div className="track-search-container">
@@ -33,6 +60,9 @@ class TrackSearch extends Component {
             }}
           >
             <i className="fa fa-search search" aria-hidden="true" />
+          </button>
+          <button type="button" onClick={this.startVoiceRecognition}>
+            <i className="fa fa-microphone" aria-hidden="true"/>
           </button>
         </form>
       </div>
